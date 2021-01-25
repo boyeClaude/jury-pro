043ebed79adff82cd5a1f3f26173cb29bbb91828
ADMIN 19 <admin19@iMac-de-ODA.local>
2021-01-25 17:50:07 +0000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: '**', component: NotFoundComponent },
+  
 ];
 
 @NgModule({
